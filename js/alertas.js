a2159a00c8a52d3ec93c4066a225503b4ad755fa
gabrielle-cascardi <gabriellecascardi52@gmail.com>
2026-08-23 22:57:26 -0300

    Corrige os defeitos apontados na revisão

--- js/alertas.js
+++ js/alertas.js
@@ -1,10 +1,16 @@
 // Alertas críticos - Linha 3
 
-var limite = 90;
+var limite = 90; // Futuramente mover para arquivo de configuração
 
 function verificarAlerta(temperatura) {
+  if (temperatura === undefined) {
+    console.log("Erro: temperatura nao informada");
+    return;
+  }
+
   if (temperatura > limite) {
     console.log("ALERTA CRITICO NA LINHA 3");
+    alert("ALERTA CRITICO NA LINHA 3");
   } else {
     console.log("Temperatura normal");
   }
